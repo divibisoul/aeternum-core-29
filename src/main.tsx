19317e275d728d19c6ae888fc9eb9901aa9fba52
divibisoul <divibi2011@gmail.com>
2026-08-25 18:05:20 -0300

    feat(mesh): start Aeternum Soul Mesh runtime at application boot

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { startSoulMeshRuntime } from "./core/mesh/SoulMeshRuntime";
 import "./index.css";
 
+startSoulMeshRuntime();
 createRoot(document.getElementById("root")!).render(<App />);
