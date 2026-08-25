72058bf180d1f149afbc0ef6206697f3eb8dcb65
divibisoul <divibi2011@gmail.com>
2026-08-25 14:07:28 -0300

    Fix: add Timber dependency

--- soul-sentinel/app/build.gradle.kts
+++ soul-sentinel/app/build.gradle.kts
@@ -26,4 +26,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("dev.rikka.shizuku:api:13.1.5")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 }
