e6f5bfdfbaa30a5e3a3965aa476e8b72eafd1931
divibisoul <divibi2011@gmail.com>
2026-08-26 06:11:37 -0300

    test(n01): enable JVM unit tests

--- soul-sentinel/app/build.gradle.kts
+++ soul-sentinel/app/build.gradle.kts
@@ -19,9 +19,7 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    kotlinOptions { jvmTarget = "17" }
     buildTypes {
         debug { isMinifyEnabled = false }
         release {
@@ -43,6 +41,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("dev.rikka.shizuku:api:13.1.5")
     implementation("com.jakewharton.timber:timber:5.0.1")
+    testImplementation(kotlin("test"))
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
 }
