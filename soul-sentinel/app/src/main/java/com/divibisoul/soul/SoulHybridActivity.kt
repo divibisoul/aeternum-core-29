ea345d303917b8d973df551ea4370a3705d72c78
divibisoul <divibi2011@gmail.com>
2026-08-26 00:02:23 -0300

    hybrid: wire bidirectional WebView Mesh completion

--- soul-sentinel/app/src/main/java/com/divibisoul/soul/SoulHybridActivity.kt
+++ soul-sentinel/app/src/main/java/com/divibisoul/soul/SoulHybridActivity.kt
@@ -3,19 +3,26 @@ package com.divibisoul.soul
 import android.os.Bundle
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
+import org.json.JSONObject
 
-/** Hosts the local hybrid Soul UI and routes UI messages into the six-nucleus runtime. */
+/** Hosts the local hybrid UI and provides bidirectional Mesh <-> WebView execution. */
 class SoulHybridActivity : ComponentActivity() {
     private lateinit var mesh: SoulMeshRuntime
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mesh = SoulMeshBootstrap.create()
-        val webView = WebView(this)
+        webView = WebView(this)
         SoulSecureWebView.configure(webView)
-        SoulHybridBridge.attach(webView, SoulHybridBridge("N01") { message ->
+        SoulHybridBridge.attach(webView, SoulHybridBridge("N01", { message ->
             mesh.send(message.source, message.target, message.capability, message.payload)
-        })
+        }, { completion ->
+            webView.post {
+                val json = JSONObject.quote(completion.toJson().toString())
+                webView.evaluateJavascript("window.SoulHybridRuntime&&window.SoulHybridRuntime.receive(JSON.parse($json));", null)
+            }
+        }))
         webView.loadUrl(SoulSecureWebView.localUrl())
         setContentView(webView)
     }
