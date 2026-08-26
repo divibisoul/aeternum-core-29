e006796d72ee535c0abeb890208c67a9d730836c
divibisoul <divibi2011@gmail.com>
2026-08-26 06:41:18 -0300

    fix(android): add ConstraintLayout dependency required by Soul shell

--- soul-sentinel/app/build.gradle.kts
+++ soul-sentinel/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.webkit:webkit:1.12.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("dev.rikka.shizuku:api:13.1.5")
     implementation("com.jakewharton.timber:timber:5.0.1")
