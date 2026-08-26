4a9a39c11637dbc75342041197b278b65e5a81dd
divibisoul <divibi2011@gmail.com>
2026-08-26 02:50:55 -0300

    runtime: enforce canonical Soul fabric topology at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { startSoulMeshRuntime } from "./core/mesh/SoulMeshRuntime";
+import { assertSoulFabricTopology } from "./core/SoulFabricContract";
 import "./index.css";
 
+// Fail fast on an invalid six-nucleus/60-channel topology. This validates structure only;
+// it intentionally does not claim that remote runtimes are connected.
+assertSoulFabricTopology();
 startSoulMeshRuntime();
 createRoot(document.getElementById("root")!).render(<App />);
