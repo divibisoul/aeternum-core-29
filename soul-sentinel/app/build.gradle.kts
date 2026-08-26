611caa1b91ce8d6a8ee801d25c43d5568e706f01
divibisoul <divibi2011@gmail.com>
2026-08-25 22:53:06 -0300

    test: enable Android instrumentation runner for Mesh transport

--- soul-sentinel/app/build.gradle.kts
+++ soul-sentinel/app/build.gradle.kts
@@ -13,6 +13,7 @@ android {
         targetSdk = 36
         versionCode = 3
         versionName = "0.3.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
@@ -27,4 +28,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("dev.rikka.shizuku:api:13.1.5")
     implementation("com.jakewharton.timber:timber:5.0.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
 }
