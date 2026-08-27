a5de4ece060cb4303048b0c0e760737e9b9ea19c
divibisoul <divibi2011@gmail.com>
2026-08-27 00:10:04 -0300

    feat(n01-mesh): expose unified peer registry facade

--- src/core/mesh/SoulMeshDiscoveryRuntime.ts
+++ src/core/mesh/SoulMeshDiscoveryRuntime.ts
@@ -1,10 +1,13 @@
 import { IndexedDBDiscoveryAdapter } from './SoulMeshDiscoveryAdapter';
 import { SoulMeshDiscoveryRegistry } from './SoulMeshDiscovery';
+import { SoulMeshPeerRegistry } from './SoulMeshPeerRegistry';
 
 /** N01's default discovery runtime: hot cache + durable browser persistence. */
 export const soulMeshDiscovery = new SoulMeshDiscoveryRegistry(
   typeof indexedDB === 'undefined' ? undefined : new IndexedDBDiscoveryAdapter(),
 );
+
+export const soulMeshPeerRegistry = new SoulMeshPeerRegistry(soulMeshDiscovery);
 
 export async function hydrateSoulMeshDiscovery(): Promise<void> {
   await soulMeshDiscovery.hydrate();
