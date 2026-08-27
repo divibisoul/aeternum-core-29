0dd9cfa9a297f4dfe67d5592da09677b53a6145f
divibisoul <divibi2011@gmail.com>
2026-08-27 01:03:42 -0300

    fix(mesh): wire canonical capability registry into N01 discovery runtime

--- src/core/mesh/SoulMeshDiscoveryRuntime.ts
+++ src/core/mesh/SoulMeshDiscoveryRuntime.ts
@@ -1,13 +1,15 @@
 import { IndexedDBDiscoveryAdapter } from './SoulMeshDiscoveryAdapter';
 import { SoulMeshDiscoveryRegistry } from './SoulMeshDiscovery';
 import { SoulMeshPeerRegistry } from './SoulMeshPeerRegistry';
+import { SoulMeshCapabilityRegistry } from './SoulMeshCapabilityRegistry';
 
 /** N01's default discovery runtime: hot cache + durable browser persistence. */
 export const soulMeshDiscovery = new SoulMeshDiscoveryRegistry(
   typeof indexedDB === 'undefined' ? undefined : new IndexedDBDiscoveryAdapter(),
 );
 
-export const soulMeshPeerRegistry = new SoulMeshPeerRegistry(soulMeshDiscovery);
+export const soulMeshCapabilityRegistry = new SoulMeshCapabilityRegistry();
+export const soulMeshPeerRegistry = new SoulMeshPeerRegistry(soulMeshDiscovery, soulMeshCapabilityRegistry);
 
 export async function hydrateSoulMeshDiscovery(): Promise<void> {
   await soulMeshDiscovery.hydrate();
