c9afe03900b2c0dcbb13c0762d76c04f4bc3d099
divibisoul <divibi2011@gmail.com>
2026-09-03 14:16:11 -0300

    refactor(n01): preserve hybrid activity while enabling Sentinel startup

--- soul-sentinel/app/src/main/java/com/divibisoul/soul/SoulHybridActivity.kt
+++ soul-sentinel/app/src/main/java/com/divibisoul/soul/SoulHybridActivity.kt
@@ -15,7 +15,9 @@ class SoulHybridActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         startSentinelIfEnabled()
-        mesh = SoulMeshBootstrap.create(webDelegate = { message -> SoulMeshBootstrap.delegateToWeb(message) })
+        mesh = SoulMeshBootstrap.create(webDelegate = { message ->
+            SoulMeshBootstrap.delegateToWeb(message)
+        })
         webView = WebView(this)
         SoulSecureWebView.configure(webView)
         SoulHybridBridge.attach(webView, SoulHybridBridge("N01", { message ->
