eb175a397d15e19bf067a9b3c7b5ca391384dd60
divibisoul <divibi2011@gmail.com>
2026-09-22 19:44:51 -0300

    feat(android): add Soul Admin core InternalEventBus.kt

---
+++ soul-sentinel/app/src/main/java/com/divibisoul/soul/core/bus/InternalEventBus.kt
@@ -0,0 +1,13 @@
+package com.divibisoul.soul.core.bus
+
+import com.divibisoul.soul.core.SoulEvent
+import com.divibisoul.soul.core.SoulEventBus
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.filterIsInstance
+
+class InternalEventBus(private val soulBus: SoulEventBus) {
+    val events: SharedFlow<SoulEvent> = soulBus.events
+    fun publish(event: SoulEvent) = soulBus.publish(event)
+    inline fun <reified T : SoulEvent> stream(): SharedFlow<T> =
+        events.filterIsInstance<T>() as SharedFlow<T>
+}
