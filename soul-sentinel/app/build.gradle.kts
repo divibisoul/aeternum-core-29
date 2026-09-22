106bb8b963d23fccf63bd572887d9a080a4fea78
divibisoul <divibi2011@gmail.com>
2026-09-22 19:53:56 -0300

    fix(android): include Shizuku provider bootstrap

--- soul-sentinel/app/build.gradle.kts
+++ soul-sentinel/app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.webkit:webkit:1.12.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("dev.rikka.shizuku:api:13.1.5")
+    implementation("dev.rikka.shizuku:provider:13.1.5")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     implementation("androidx.room:room-runtime:2.7.0")
