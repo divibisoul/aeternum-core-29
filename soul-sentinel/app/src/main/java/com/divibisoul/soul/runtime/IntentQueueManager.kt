04351d951a4e8d97d9315921ab9f9cb98880aeb7
divibisoul <divibi2011@gmail.com>
2026-09-22 20:12:56 -0300

    chore(android): remove unused queue import

--- soul-sentinel/app/src/main/java/com/divibisoul/soul/runtime/IntentQueueManager.kt
+++ soul-sentinel/app/src/main/java/com/divibisoul/soul/runtime/IntentQueueManager.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
