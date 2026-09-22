1b5f035ebb1dd610d2734dcc45e87687da2f05de
divibisoul <divibi2011@gmail.com>
2026-09-22 19:48:54 -0300

    feat(android): add biometric credential gate dependency

--- soul-sentinel/app/build.gradle.kts
+++ soul-sentinel/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.1.2")
     implementation("androidx.work:work-runtime-ktx:2.10.1")
     implementation("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.github.topjohnwu.libsu:core:6.0.0")
     implementation("org.tensorflow:tensorflow-lite:2.17.0")
