fc65db5e04eda066b58c2bbb79a806e99823cecb
divibisoul <divibi2011@gmail.com>
2026-09-23 07:12:56 -0300

    fix(aeternum): complete distributed M8 governance hub

--- lib/aeternum/governance/index.ts
+++ lib/aeternum/governance/index.ts
@@ -4,6 +4,7 @@ export { VagusNerveReportUI } from "./VagusNerveReportUI";
 export { unifiedCommandPipelineModule, UnifiedCommandPipelineModule } from "./UnifiedCommandPipelineModule";
 export { systemIntegrationModule, SystemIntegrationModule } from "./SystemIntegrationModule";
 export { architectureGuideModule, ArchitectureGuideModule } from "./ArchitectureGuideModule";
+export { memoryModule, MemoryModule } from "./MemoryModule";
 
 export const AETERNUM_GOVERNANCE_NETWORK = Object.freeze([
   { id: "M8.vagus-nerve-service", owner: "N01", authority: "M1_CORE" },
@@ -14,6 +15,5 @@ export const AETERNUM_GOVERNANCE_NETWORK = Object.freeze([
   { id: "M8.governance-report", owner: "N07_SARA", authority: "GovernanceBackend" },
   { id: "M8.enforcement-pipeline", owner: "N07_SARA", authority: "GovernedSARA" },
   { id: "M8.memory", owner: "N07_SARA", authority: "RegenerativeMemory" },
-  { id: "M8.vagus-service", owner: "N01", authority: "M1_CORE" },
   { id: "M8.orientation-guide", owner: "N06", authority: "M4_MIND" },
 ] as const);
